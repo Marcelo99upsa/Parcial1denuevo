b2c6b18f09d0d25890d9d3d8d84651e8deab2a65
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-03-20 10:39:03 -0400

    Model Product Creado

---
+++ ApiProduct/ApiProduct/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiProduct.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(1, 99999)]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Description { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
+        public float Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime LastBuy { get; set; }
+    }
+}
